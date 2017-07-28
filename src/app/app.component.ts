ed20d71689640ffc7adaa7a63db270e74c5146b7
Beth Cummings <bethios@gmail.com>
2017-07-28 14:43:30 -0500

    ufo comet and rocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   scrollTo(element){
     let orbit = element + "-orbit";
     let location = document.getElementsByClassName(element)[0];
-    
+
     if(element !== 'sun'){
       document.getElementById(orbit).style.borderStyle = 'solid';
       document.getElementById(orbit).style.borderColor = 'white';
@@ -21,30 +21,21 @@ export class AppComponent {
         document.getElementById(this.lastSelectedOrbit).style.borderStyle = 'dashed';
         document.getElementById(this.lastSelectedOrbit).style.borderColor = '#4A437F';
       }
-
       this.lastSelectedOrbit = orbit;
     }
 
     location.scrollIntoView();
     let body = document.body;
     let html = document.documentElement;
-    body.scrollTop -= 200;
-    html.scrollTop -= 200;
+    body.scrollLeft += 100;
+    html.scrollLeft += 100;
+    body.scrollTop -= 100;
+    html.scrollTop -= 100;
   }
 
   ngOnInit() {
-      let body = document.body;
-      let html = document.documentElement;
-      body.scrollLeft = 500;
-      body.scrollTop = 570;
-      html.scrollLeft = 500;
-      html.scrollTop = 570;
-  }
-
-  changeZoom(value) {
-    console.log(value);
-    document.getElementById('circle-orbit-container').style.transform = 'scale({{value}})';
-
+    let location = document.getElementsByClassName('sun')[0];
+    location.scrollIntoView();
   }
 
 }
